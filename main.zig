977923e90ed7c8a08aac102a415bf7a9495cbdb1
kadekillary <kadekillary@pm.me>
2025-03-24 02:31:03 -0500

    it lives

--- main.zig
+++ main.zig
@@ -1,31 +1,12 @@
 const std = @import("std");
+const print = std.debug.print;
+const Child = std.process.Child;
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
-    const scrcpyArgs = &[_][]const u8{
-        "scrcpy",
-        "--max-size", "1024",
-        "--bit-rate", "4M",
-        "--turn-screen-off",
-        "--show-touches",
-    };
-
-    // --turn-screen-off
-    // --stay-awake (prevent device from sleeping)
-
-    var scrcpyChild = std.process.Child.init(scrcpyArgs, allocator);
-    scrcpyChild.stdout_behavior = .Inherit;
-    scrcpyChild.stderr_behavior = .Inherit;
-    try scrcpyChild.spawn();
-
-    std.time.sleep(2 * std.time.ns_per_s);
-
-    // list all apps
-    // scrcpy --list-apps
-
-    // start app on launch
-    // scrcpy --start-app=org.mozilla.firefox
-
-    _ = try scrcpyChild.wait();
+    const scrcpyArgs = [_][]const u8{ "scrcpy", "--start-app=jp.pokemon.pokemontcgp" };
+    const result = try Child.run(.{
+        .allocator = std.heap.page_allocator,
+        .argv = &scrcpyArgs,
+    });
+    print("{s}\n", .{result.stdout});
 }
