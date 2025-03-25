94d923f0591853316f2cb71c44471b2e4a950172
kadekillary <kadekillary@pm.me>
2025-03-25 07:45:15 -0500

    increase time, click ctxt

--- main.zig
+++ main.zig
@@ -9,10 +9,10 @@ pub fn main() !void {
     var device = try andy.Andy.init(allocator);
     defer device.deinit();
 
-    try device.tap(719, 2288);
-    std.time.sleep(1_000_000_000);
-    try device.tap(260, 1912);
-    std.time.sleep(1_000_000_000);
-    try device.tap(739, 1947);
-    std.time.sleep(1_000_000_000);
+    try device.tap(719, 2288); // battle icon
+    std.time.sleep(10_000_000_000); // 10 seconds (in ns)
+    try device.tap(260, 1912); // versus icon
+    std.time.sleep(10_000_000_000);
+    try device.tap(739, 1947); // random match icon
+    std.time.sleep(10_000_000_000);
 }
